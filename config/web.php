70a35b91b08c2749cc10b2c7ac7f91ef38a169cc
Ruslan Kiricenko <ruslankus@yahoo.com>
2016-09-22 23:35:44 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -41,8 +41,8 @@ $config = [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'startmobile1970',
-                'password' => 'starter1970',
+                'username' => '********',
+                'password' => '********',
                 'port' => '465', // Port 25 is a very common port too
                 'encryption' => 'ssl', // It is often used, check your provider or mail server specs
             ],
